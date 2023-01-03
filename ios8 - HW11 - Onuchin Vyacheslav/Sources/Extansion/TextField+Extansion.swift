ae11fa3fd4c7416b4eed210eed3767e0d222808c
Vyacheslav Onuchin <sla651@mail.ru>
2023-01-03 21:48:18 +1000

    Added two textFields and two buttons, edited extensions for textFields and buttons

--- ios8 - HW11 - Onuchin Vyacheslav/Sources/Extansion/TextField+Extansion.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TextField+Extansion.swift
-//  ios8 - HW11 - Onuchin Vyacheslav
-//
-//  Created by Вячеслав Онучин on 27.12.2022.
-//
-
-import UIKit
--- ios8 - HW11 - Onuchin Vyacheslav/Sources/ViewController.swift
+++ ios8 - HW11 - Onuchin Vyacheslav/Sources/ViewController.swift
@@ -11,7 +11,6 @@ class ViewController: UIViewController {
         return imageView
     }()
 
-
     private lazy var label: UILabel = {
         let label = UILabel()
         label.text = "Login"
@@ -19,6 +18,59 @@ class ViewController: UIViewController {
         label.font = UIFont.systemFont(ofSize: 40, weight: .bold)
         label.textAlignment = .center
         return label
+    }()
+
+    private lazy var login: UITextField = {
+        let login = UITextField()
+        login.placeholder = "Enter your username"
+        login.textColor = .black
+        login.textAlignment = .left
+        login.backgroundColor = .white
+        login.font = UIFont(name: "Inter-Regular", size: 14)
+        login.layer.cornerRadius = 25
+        login.setLeftIconLogin(UIImage(systemName: "person"))
+        login.setRightIcon(UIImage(systemName: "checkmark.circle.fill"))
+        login.shadowSattings(login)
+        return login
+    }()
+
+    private lazy var password: UITextField = {
+        let password = UITextField()
+        password.placeholder = "Enter your password"
+        password.font = UIFont(name: "Inter-Regular", size: 14)
+        password.textColor = .black
+        password.layer.cornerRadius = 25
+        password.textAlignment = .left
+        password.backgroundColor = .white
+        password.isSecureTextEntry = true
+        password.clearButtonMode = .always
+        password.setLeftIconPassword(UIImage(systemName: "lock"))
+        password.shadowSattings(password)
+        return password
+    }()
+
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(UIColor.gray, for: .normal)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 25
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+        button.backgroundColor = .clear
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        return button
+    }()
+
+    private lazy var recoverPassword: UIButton = {
+        let recoverPassword = UIButton()
+        recoverPassword.setTitle("Forgot your password?", for: .normal)
+        recoverPassword.setTitleColor(UIColor.gray, for: .normal)
+        recoverPassword.backgroundColor = UIColor.clear
+        recoverPassword.clipsToBounds = true
+        recoverPassword.layer.cornerRadius = 25
+        recoverPassword.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        return recoverPassword
     }()
 
     // MARK: - LifeCycle
@@ -34,6 +86,10 @@ class ViewController: UIViewController {
     private func setupHierarchy() {
         view.addSubview(image)
         view.addSubview(label)
+        view.addSubview(login)
+        view.addSubview(password)
+        view.addSubview(button)
+        view.addSubview(recoverPassword)
 
     }
 
@@ -43,12 +99,48 @@ class ViewController: UIViewController {
         }
 
         label.snp.makeConstraints { make in
-            make.centerY.equalTo(view).offset(-250)
+            make.centerY.equalTo(view).offset(-300)
             make.centerX.equalTo(view)
+        }
+
+        login.snp.makeConstraints { make in
+            make.top.equalTo(label.snp.bottom).offset(20)
+            make.centerX.equalTo(view)
+            make.height.equalTo(50)
+            make.left.equalTo(40)
+            make.right.equalTo(-40)
+        }
+
+        password.snp.makeConstraints { make in
+            make.top.equalTo(login.snp.bottom).offset(20)
+            make.centerX.equalTo(view)
+            make.height.equalTo(50)
+            make.left.equalTo(40)
+            make.right.equalTo(-40)
+        }
+
+        button.snp.makeConstraints { make in
+            make.top.equalTo(password.snp.bottom).offset(50)
+            make.centerX.equalTo(view)
+            make.height.equalTo(50)
+            make.left.equalTo(40)
+            make.right.equalTo(-40)
+        }
+
+        recoverPassword.snp.makeConstraints { make in
+            make.top.equalTo(button.snp.bottom).offset(65)
+            make.centerX.equalTo(view)
+            make.height.equalTo(50)
+            make.left.equalTo(40)
+            make.right.equalTo(-40)
         }
     }
 
     // MARK: -Actions
+    
+    @objc private func buttonPressed() {
+        print("Функция работает")
+    }
 
 }
 
